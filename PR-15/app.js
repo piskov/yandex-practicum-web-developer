aed064ea8e7065ff9224bf4bf39d1dcc8078be13
Andrey Piskov <andrey.piskov@gmail.com>
2020-01-31 12:48:48 +0300

    Add crash-test endpoint

--- PR-15/app.js
+++ PR-15/app.js
@@ -32,6 +32,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(requestLogger);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.post('/signin',
   celebrate({
     body: Joi.object().keys({
